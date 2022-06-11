cd96189134f5403389bdd8ad0e3dbdd2dbf340a5
fred-fittywebdev <contact@fittywebdev.fr>
2022-06-11 17:10:02 +0200

    Mise en place du projet, création du dossier backend, initialisation du serveur et mise en place de la structure des dossiers dans le backend

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+// dotenv
+dotenv.config()
+
+// mongodb+srv://Fred:fittywebdev@cluster0.f76fu0e.mongodb.net/?retryWrites=true&w=majority
+
+const app = express()
+
+//middleware
+app.use(morgan('dev'))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    app.listen(PORT, () =>
+        console.log(`Server is listening at ${PORT} and database connection established`))
+}).catch((error) => console.log(`${error} connexion failed`))
