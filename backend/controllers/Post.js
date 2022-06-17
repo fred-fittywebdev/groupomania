1d69a9dfdb3992e0ce713637aadb42a31c3b49c3
fred-fittywebdev <contact@fittywebdev.fr>
2022-06-17 13:45:09 +0200

    route et controller pour afficher un post unique, effacer et modifier un post; OK

--- backend/controllers/Post.js
+++ backend/controllers/Post.js
@@ -39,3 +39,56 @@ export const getPost = async (req, res) => {
         res.status(404).json({ message: 'Une erreur est survenue.' })
     }
 }
+
+// Récupérer un post spécifique a un utilsateur pour le dashboard
+export const getPostsByUser = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: 'Cet utilisateur n\'existe pas' })
+    }
+    const userPosts = await PostModel.find({ creator: id })
+    res.status(200).json(userPosts)
+}
+
+// Supprimer un post
+export const deletePost = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ message: `l' ${id} ne correspond a aucun post enregistré.` })
+        }
+
+        await PostModel.findByIdAndRemove(id)
+        res.json({ message: 'Le posta à bien été supprimé' })
+    } catch (error) {
+        res.status(404).json({ message: 'Une erreur est survenue.' })
+    }
+}
+
+// MODIFIER un post
+export const updatePost = async (req, res) => {
+    const { id } = req.params
+    const { title, content, creator, imageFile, tags } = req.body
+
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ message: `l' ${id} ne correspond a aucun post enregistré.` })
+        }
+
+        const updatedPost = {
+            creator,
+            title,
+            content,
+            tags,
+            imageFile,
+            _id: id
+        }
+
+        await PostModel.findByIdAndUpdate(id, updatedPost, { new: true })
+        res.json(updatedPost)
+    } catch (error) {
+        res.status(404).json({ message: 'Une erreur est survenue.' })
+    }
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -2,11 +2,14 @@ import express from 'express';
 import auth from '../middleware/auth.js';
 const router = express.Router();
 
-import { createPost, getPost, getPosts } from '../controllers/Post.js';
+import { createPost, getPost, getPostsByUser, getPosts, deletePost, updatePost } from '../controllers/Post.js';
 
 router.post('/', auth, createPost)
 router.get('/', getPosts)
 router.get('/:id', getPost)
+router.delete('/:id', auth, deletePost)
+router.patch('/:id', auth, updatePost)
+router.get('/userPosts/:id', auth, getPostsByUser)
 
 
 
